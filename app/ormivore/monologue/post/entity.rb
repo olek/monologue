5b8ccf93cc17e9d1b8817531b6b374878ec64c48
Olek Poplavsky <olek@woodenbits.com>
2014-01-29 21:51:46 -0500

    move post associations to entity

--- app/ormivore/monologue/post/entity.rb
+++ app/ormivore/monologue/post/entity.rb
@@ -12,7 +12,9 @@ module Monologue
         user_id: Integer
       )
 
-      associations Associations
+      many_to_one :user, User::Entity, fk: 'user_id'
+      one_to_many :taggings, Tagging::Entity, fk: 'post_id'
+      many_to_many :tags, Tag::Entity, through: 'taggings', fk: 'tag_id'
 
       responsibility :questions, Questions
       responsibility :actions, Actions
--- app/ormivore/monologue/user/repo.rb
+++ app/ormivore/monologue/user/repo.rb
@@ -4,6 +4,15 @@ module Monologue
       include ORMivore::Repo
 
       self.default_entity_class = Entity
+
+      def find_by_name(name)
+        entities_attrs = port.find(
+          { name: name },
+          [:id].concat(entity_class.attributes_list),
+          limit: 1
+        )
+        entities_attrs.map { |ea| attrs_to_entity(ea) }.first
+      end
     end
   end
 end
