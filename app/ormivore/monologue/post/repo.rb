cb40aa7097adda81588366de7ff217029b1d7133
Olek Poplavsky <olek@woodenbits.com>
2014-03-09 19:36:59 -0400

    deleting incidental join entities is now ORMivore's responsibility

--- app/ormivore/monologue/post/repo.rb
+++ app/ormivore/monologue/post/repo.rb
@@ -3,15 +3,6 @@ module Monologue
     class Repo
       include ORMivore::Repo
 
-      def delete(post)
-        # TODO not the most efficient way to destroy/delete taggings, may do in one query
-        post.associations.taggings.each do |tagging|
-          family[Tagging::Entity].delete(tagging)
-        end
-
-        super
-      end
-
       find :all, order: { published_at: :descending, id: :descending }, named: 'for_listing'
       find :all, by: 'user_id'
     end
